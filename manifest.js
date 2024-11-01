884cef931c7b63baa2438bb3d3775dd749bde2d1
muhammedfettahu <122065669+muhammedfettahu@users.noreply.github.com>
2024-11-01 18:28:19 +0100

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+    "name": "Music Player",
+    "short_name": "Music Player",
+    "start_url": "./index.html",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#000000",
+    "icons": [
+        {
+            "src": "icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        }
+    ]
+}
